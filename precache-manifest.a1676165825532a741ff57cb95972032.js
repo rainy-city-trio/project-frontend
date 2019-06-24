e094d62c5047edad0ecadc4626fdcb6ff6518b36
etfeire <etfeire@gmail.com>
2019-06-24 16:09:12 +0100

    Updates

---
+++ precache-manifest.a1676165825532a741ff57cb95972032.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e43265fa750df5ee5a6418ee25cd44a4",
+    "url": "/rainy-city-trio/project-frontend/index.html"
+  },
+  {
+    "revision": "ab267bc255f706602937",
+    "url": "/rainy-city-trio/project-frontend/static/css/main.c79dc271.chunk.css"
+  },
+  {
+    "revision": "ca3efbc27fab26835de6",
+    "url": "/rainy-city-trio/project-frontend/static/js/2.58a6bbed.chunk.js"
+  },
+  {
+    "revision": "ab267bc255f706602937",
+    "url": "/rainy-city-trio/project-frontend/static/js/main.7769f7c1.chunk.js"
+  },
+  {
+    "revision": "ae0cfd2505ebd6c35021",
+    "url": "/rainy-city-trio/project-frontend/static/js/runtime~main.a7140952.js"
+  },
+  {
+    "revision": "b3e8a6dc42d3bfbcb7ada2a34335b585",
+    "url": "/rainy-city-trio/project-frontend/static/media/cropped33.b3e8a6dc.png"
+  }
+]);
